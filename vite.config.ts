db8928d030ce917741f8a649502dbc95e619a771
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2023-06-24 23:46:17 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/paastech-test',
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
